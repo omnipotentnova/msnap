bda08b8cd8dbb4375e5ce5edd26eee35d3c9c0dd
Kevin Wilson <khwilson@gmail.com>
2013-02-17 23:17:48 -0500

    Added some questions from the spreadsheet

--- html/snap_questions.js
+++ html/snap_questions.js
@@ -1,23 +1,27 @@
 'use strict';
 
-var firstQuestion = "name"; // The key of the first question to be asked
+var CONSTANTS = {
+  "poverty": [ // The maximum amount of money a family of size idx
+               // can make per month 
+    0, 932, 1262, 1592, 1922, 2252, 2582, 2912, 3242, 3572, 3902, 4232, 4562
+  ],
+  "maximumMonthlyBenefit": [ // Maximum SNAP benefit for a household of size (key)
+    0, 200, 367, 526, 668, 793, 952, 1052, 1202, 1352, 1502, 1652, 1802
+  ]
+};
+
+var firstQuestion = "people"; // The key of the first question to be asked
 var questions = { // The actual questions to be asked in json
-  "name": {
+  "people": {
     "fields": [
-      {"name": "myname",
-       "type": "string",
-       "text": "What is your name?"}
-    ],
-    "next": function() {
-      return "int";
-    }
-  },
-  "int": {
-    "fields": [
-      {"name": "myint",
+      {"name": "totalNumber",
        "type": "int",
-       "required": false,
-       "text": "What is your favorite nonnegative integer?"}
+       "required": true,
+       "text": "How many people are in your household?"},
+      {"name": "elderlyOrDisabled",
+       "type": "yesorno",
+       "required": true,
+       "text": "Is anyone in your household elderly or disabled?"}
     ],
     "next": function() {
       return "DONE";
@@ -31,6 +35,6 @@ var questions = { // The actual questions to be asked in json
  * displayed in the results associative array.
  */
 function computeResults() {
-  results.push({"name": "myname", "label": "NAME", "text": answers.name.myname});
-  results.push({"name": "myint", "label": "INT", "text": answers.int.myint});
+  results.push({"name": "peopleInHousehold", "label": "PEEPS", "text": answers.people.totalNumber});
+  results.push({"name": "elderlyOrDisabled", "label": "HELP", "text": answers.people.elderlyOrDisabled});
 }
